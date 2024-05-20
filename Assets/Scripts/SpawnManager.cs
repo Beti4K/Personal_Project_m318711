7b076f6844837e19e7b64f16288ca60b7c4e8a34
Beti4K <beata.kolodziejczyk2004@gmail.com>
2024-05-20 10:44:12 +0100

    SpawnEnemies
    Enemies spawn on radius around the player

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     private int border = 21;
+    private int radius;
     [SerializeField] GameObject obstacle;
     [SerializeField] GameObject enemy;
     void Start()
     {
         StartCoroutine(SpawnObstacle());
+        StartCoroutine(SpawnEnemy());
     }
 
     IEnumerator SpawnObstacle()
     {
-        Instantiate(obstacle, new Vector3(Random.Range(-border, border), 0.5f, Random.Range(-border, border)), transform.rotation);
+        Instantiate(obstacle, new Vector3(Random.Range(-border, border), 0.5f, Random.Range(-border, border)), Quaternion.identity);
         yield return new WaitForSeconds(5);
         StartCoroutine(SpawnObstacle());
     }
 
     IEnumerator SpawnEnemy()
     {
-        Instantiate(enemy, new Vector3(Random.Range(-border, border), 0.5f, Random.Range(-border, border)), transform.rotation);
-        yield return new WaitForSeconds(5);
+        //Setting spawn direction
+        Vector3 spawnDirection = Random.onUnitSphere;
+        spawnDirection.y = 0;
+        spawnDirection.Normalize();
+
+        //Setting spawn position based on direction + player position
+        radius = Random.Range(3, 6);
+        Vector3 spawnPosition = GameObject.Find("Player").transform.position + spawnDirection * radius;
+
+        Instantiate(enemy, spawnPosition, Quaternion.identity);
+        yield return new WaitForSeconds(2);
         StartCoroutine(SpawnEnemy());
     }
 }
